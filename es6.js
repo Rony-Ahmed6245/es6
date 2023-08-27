76a54d6107912ff39a85119ea08eba5c6418ccc5
ronyahmed6245 <ronyahmed6245.gmail.com>
2023-08-27 16:00:57 +0600

    fast commit

---
+++ es6.js
@@ -0,0 +1,56 @@
+// ternary oparetor 
+// const marks = 60;
+// if(marks >= 50){
+//     console.log('scic')
+// }else{
+//     console.log('miss scic')
+// }
+// marks >= 50? console.log('scic') : console.log('miss')
+// =========================================================
+
+//reference out 
+//max number give him
+// array in number
+// const arr = [3,4,5,6];
+// const arr2 = [...arr, 7 ,8];
+// console.log(arr)
+// console.log(arr2)
+
+// const maxNum =Math.max(...arr)
+// console.log(maxNum)
+
+
+// const arrSum = (arr3)=>{
+//     let newArr = 0;
+//     for(const item of arr3){
+//         newArr =newArr +  item ;  
+//     }
+//     return newArr ;
+// }
+
+// const arr3 = [1, 3, 8];
+// const sum = arrSum(arr3);
+// console.log(sum)
+
+
+// const person = {
+//     name:"mehedy",
+//     age: 26
+// }
+
+// const person2 = {...person , city : 'dhaka'};
+// person2.address = "Rajshahi";
+// console.log(person)
+// console.log(person2)
+
+
+
+//  const person = {
+//         name:"mehedy",
+//         age: 26,
+//         city: 'dhaka'
+//     }
+
+//     for( const item in person){
+//         console.log(item)
+//     }
